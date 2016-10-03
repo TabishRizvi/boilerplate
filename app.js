a604fe4466d8b7e885102527e2210bdede24b1eb
Tabish Rizvi <tabish.haidar@click-labs.com>
2016-10-03 11:06:27 +0530

    x1

--- app.js
+++ app.js
@@ -4,17 +4,35 @@
  * Email : sayyidtabish@gmail.com
  */
 
-var express = require("express"),
-    bodyParser = require("body-parser");
+config = require("config");                         // provides global variable config
+require("./lib/logging");                           // provides global variable logger
 
+var express = require("express"),
+    bodyParser = require("body-parser"),
+    morgan = require("morgan");
 
 var app = express();
 
 app.use([
+    morgan("tiny", { "stream": logger.stream }),
     bodyParser.json(),
     bodyParser.urlencoded()
 ]);
 
-config = require("config");
 
-console.log(config.get("port"));
+
+app.use(function (req,res) {
+    console.log(req.headers);
+    console.log(req.host);
+    console.log(req.origin);
+    res.set("Location","http://facebook.com");
+    res.status(301).send("hi2");
+});
+
+
+app.set('port',config.get("port"));
+app.set('etag',false);
+
+app.listen(app.get('port'),function(){
+    console.log("Server running on port\t===============>\t",app.get('port'));
+});
